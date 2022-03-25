b51a9828d4d14b9589ec4f34dc93de8106387660
n_leonov <65124365+n-leonov@users.noreply.github.com>
2022-03-26 00:15:58 +0300

    Initial Commit

---
+++ Navigation-View/Navigation_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_ViewApp.swift
+//  Navigation-View
+//
+//  Created by Macbook on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
